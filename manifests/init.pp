e790bc24db32c0c271242a6c30c2bb031ad6a5bf
David RACODON <david.racodon@gmail.com>
2017-06-12 16:22:14 +0200

    Update SonarQube default version to latest LTS (5.6.6)

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 class sonarqube (
-  $version          = '4.5.5',
+  $version          = '5.6.6',
   $user             = 'sonar',
   $group            = 'sonar',
   $user_system      = true,
