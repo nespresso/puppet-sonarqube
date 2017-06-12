2c65550070b1e1fdc6e80445bc88ad8a5e04d522
David RACODON <david.racodon@gmail.com>
2017-06-12 16:26:44 +0200

    Remove useless SonarQube Runner JDBC configuration

--- manifests/runner.pp
+++ manifests/runner.pp
@@ -6,12 +6,7 @@ class sonarqube::runner (
   $version          = '2.4',
   $download_url     = 'http://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist',
   $installroot      = '/usr/local',
-  $sonarqube_server = 'http://sonar.local:9000/',
-  $jdbc             = {
-    url      => 'jdbc:h2:tcp://localhost:9092/sonar',
-    username => 'sonar',
-    password => 'sonar',
-  },
+  $sonarqube_server = 'http://localhost:9000/',
 ) {
   validate_string($package_name)
   validate_absolute_path($installroot)
@@ -31,7 +26,6 @@ class sonarqube::runner (
     package_name     => $package_name,
     version          => $version,
     installroot      => $installroot,
-    jdbc             => $jdbc,
     sonarqube_server => $sonarqube_server,
   } ~>
   anchor { 'sonarqube::runner::end': }
--- manifests/runner/config.pp
+++ manifests/runner/config.pp
@@ -4,11 +4,6 @@ class sonarqube::runner::config (
   $version,
   $installroot,
   $sonarqube_server = 'http://localhost:9000',
-  $jdbc             = {
-    url      => 'jdbc:h2:tcp://localhost:9092/sonar',
-    username => 'sonar',
-    password => 'sonar',
-  },
 ) {
   # Sonar Runner configuration file
   file { "${installroot}/${package_name}-${version}/conf/sonar-runner.properties":
