a2d80567ace36ab67e9744b1ce01df3b28fa3b73
David RACODON <david.racodon@gmail.com>
2017-06-12 16:20:53 +0200

    Make Compute Engine properties parametrizable

--- manifests/init.pp
+++ manifests/init.pp
@@ -49,6 +49,8 @@ class sonarqube (
   $search_java_opts = undef,
   $search_host      = '127.0.0.1',
   $search_port      = '9001',
+  $ce_java_opts     = undef,
+  $ce_worker_count  = undef,
   $config           = undef,
 ) inherits sonarqube::params {
   validate_absolute_path($download_dir)
